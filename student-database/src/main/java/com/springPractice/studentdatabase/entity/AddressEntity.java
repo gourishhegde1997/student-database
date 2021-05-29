21c2b504d7343dbc947711be7e8f350dce99d39b
Gourisha Hegde <gourishdhegde@gmail.com>
2021-05-29 14:30:46 +0530

    tried reformatting of file for AddressEntity

--- student-database/src/main/java/com/springPractice/studentdatabase/entity/AddressEntity.java
+++ student-database/src/main/java/com/springPractice/studentdatabase/entity/AddressEntity.java
@@ -1,70 +1,64 @@
 package com.springPractice.studentdatabase.entity;
 
-import java.util.UUID;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
 import com.springPractice.studentdatabase.dto.AddressDTO;
+
+import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "address", schema = "student_db")
 //@GenericGenerator(name = "id_gen", strategy = "increment")
 public class AddressEntity {
-	
-	@Id
-	@Column(name = "address_id")
+
+    @Id
+    @Column(name = "address_id")
 //	@GeneratedValue(generator = "id_gen")
-	@GeneratedValue
-	private UUID addressId;
-	
-	@Column(name = "address_details")
-	private String addressDetails;
-	
-	@Column(name = "postal_code")
-	private Integer postalCode;
+    @GeneratedValue
+    private UUID addressId;
 
-	public UUID getAddressId() {
-		return addressId;
-	}
+    @Column(name = "address_details")
+    private String addressDetails;
 
-	public void setAddressId(UUID addressId) {
-		this.addressId = addressId;
-	}
+    @Column(name = "postal_code")
+    private Integer postalCode;
 
-	public String getAddressDetails() {
-		return addressDetails;
-	}
+    public AddressEntity(UUID addressId, String addressDetails, Integer postalCode) {
+        super();
+        this.addressId = addressId;
+        this.addressDetails = addressDetails;
+        this.postalCode = postalCode;
+    }
 
-	public void setAddressDetails(String addressDetails) {
-		this.addressDetails = addressDetails;
-	}
+    public AddressEntity() {
+        super();
+    }
 
-	public Integer getPostalCode() {
-		return postalCode;
-	}
+    public static AddressDTO getDtoObject(AddressEntity address) {
+        return new AddressDTO(address.getAddressId(), address.getAddressDetails(), address.getPostalCode());
+    }
 
-	public void setPostalCode(Integer postalCode) {
-		this.postalCode = postalCode;
-	}
-	
-	public static AddressDTO getDtoObject(AddressEntity address) {
-		return new AddressDTO(address.getAddressId(), address.getAddressDetails(), address.getPostalCode());
-	}
+    public UUID getAddressId() {
+        return addressId;
+    }
 
-	public AddressEntity(UUID addressId, String addressDetails, Integer postalCode) {
-		super();
-		this.addressId = addressId;
-		this.addressDetails = addressDetails;
-		this.postalCode = postalCode;
-	}
+    public void setAddressId(UUID addressId) {
+        this.addressId = addressId;
+    }
 
-	public AddressEntity() {
-		super();
-	}
-	
+    public String getAddressDetails() {
+        return addressDetails;
+    }
+
+    public void setAddressDetails(String addressDetails) {
+        this.addressDetails = addressDetails;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
 }
